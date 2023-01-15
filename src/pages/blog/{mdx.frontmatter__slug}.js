692f347c504d450166d6d74156e9bb83538bc566
Heri Setiawan <heri15002@mail.unpad.ac.id>
2023-01-16 02:17:11 +0700

    update

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import { graphql } from "gatsby";
 import Layout from "../../components/layout";
 import { Seo } from "../../components/seo";
-import { GatsbyImage } from "gatsby-plugin-image";
 
 const BlogPost = ({ data, children }) => {
   const post = data.mdx.frontmatter;
@@ -10,7 +9,6 @@ const BlogPost = ({ data, children }) => {
   return (
     <Layout pageTitle={post.title}>
       <article className="w-[400px] p-[8px] font-sans text-[13px]">
-        <GatsbyImage image={post.thumbnail} />
         <h1>{post.title}</h1>
         <p>{post.date}</p>
         {children}
@@ -25,7 +23,6 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
-        thumbnail
       }
     }
   }
